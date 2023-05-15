94fdfa66d0479d3ff195fafe339227ed98c35e3e
AdalRodNun <adalberto.rn@outlook.com>
2023-05-14 23:19:13 -0600

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Website Lab 3`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms"]
+};
